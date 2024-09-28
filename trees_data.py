7e9ff63cb71f07b323fffa59152e300a9150243d
Skwarson96 <maciej.skwara2@gmail.com>
2024-09-28 15:25:24 +0200

    finish parki and lasy data read

--- trees_data.py
+++ trees_data.py
@@ -61,6 +61,58 @@ def read_pdf_data(pdf_file_path):
     return all_text
 
 
+def create_lasy_df(txt_content):
+    # Szukamy wzorca dla powierzchni lasów
+    match = re.search(r'Informacja o powierzchni lasów w Krakowie: (\d+,\d+) ha', txt_content)
+
+    if match:
+        # Zamieniamy znalezioną powierzchnię na float, zamieniając przecinek na kropkę
+        powierzchnia = float(match.group(1).replace(",", "."))
+
+        # Tworzymy DataFrame z odpowiednimi kolumnami
+        df = pd.DataFrame({
+            'INDEX': [1],  # Stały indeks
+            'MIASTO': ['Kraków'],  # Miasto: Kraków
+            'POW (ha)': [powierzchnia]  # Powierzchnia lasów w ha
+        })
+
+        return df
+    else:
+        # Jeśli nie znajdziemy wzorca, zwracamy pusty DataFrame
+        return pd.DataFrame(columns=['INDEX', 'MIASTO', 'POW (ha)'])
+
+
+def create_parki_krak_df(dane_txt):
+    # Podziel dane na linie
+    lines = dane_txt.strip().split('\n')
+
+    # Lista do przechowywania danych
+    data = []
+
+    # Przetwarzanie każdej linii
+    for i, line in enumerate(lines, start=1):
+        # Wyciągnięcie nazwy ulicy
+        ulica_match = re.match(r'(.+?)\s*[-–]\s*', line)
+        if ulica_match:
+            ulica = ulica_match.group(1).strip()
+
+        # Wyciągnięcie liczb (dodawanie drzew, jeśli jest więcej niż jedna liczba)
+        liczby = re.findall(r'(\d+)\s*drzew', line)
+        liczba_drzew = sum(map(int, liczby))  # Suma drzew
+
+        # Dodajemy dane do listy
+        data.append([i, ulica, liczba_drzew])
+
+    # Tworzenie DataFrame
+    df = pd.DataFrame(data, columns=['INDEX', 'ULICA', 'LICZBA_DRZEW'])
+
+    return df
+
+
+def read_txt_data(txt_file_path):
+    with open(txt_file_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+    return content
 
 def main():
 
@@ -72,7 +124,18 @@ def main():
     parki_kiesz_data = read_pdf_data(parki_kiesz_pdf_file_path)
     parki_kiesz_df = create_parki_df(parki_kiesz_data)
 
-    print(parki_kiesz_df)
+
+    lasy_txt_file_path = "./green_data/krakow_lasy.txt"
+    lasy_data = read_txt_data(lasy_txt_file_path)
+    lasy_df = create_lasy_df(lasy_data)
+
+    parki_krako_txt_file_path = "./green_data/parki_krakowian.txt"
+    parki_krak_data = read_txt_data(parki_krako_txt_file_path)
+    parki_krak_df = create_parki_krak_df(parki_krak_data)
+
+
+    print(parki_krak_df)
+
 
 if __name__ == '__main__':
     main()
